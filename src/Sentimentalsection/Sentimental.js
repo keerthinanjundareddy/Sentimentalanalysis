030c821e2c7af002a9e47b931ef062cd65600287
keerthinanjundareddy <keerthana@apprikart.com>
2023-08-29 15:32:34 +0530

    mes

--- src/Sentimentalsection/Sentimental.js
+++ src/Sentimentalsection/Sentimental.js
@@ -1,48 +1,63 @@
-import React, { useState } from 'react'
-import './Sentimental.css'
+import React, { useState } from 'react';
+import './Sentimental.css';
 
 function Sentimental() {
-    const[userinput,setUserinput]=useState()
-    const[userOutput,setUseroutput]=useState()
+  const [userinput, setUserinput] = useState('');
+  const [userOutput, setUserOutput] = useState('');
 
-    console.log("userinput",userinput)
-    const inputchange = (e) =>{
-        setUserinput(e.target.value);
-        console.log("userinput",userinput)
+  console.log('userinput', userinput);
+
+  const inputchange = (e) => {
+    setUserinput(e.target.value);
+  }
+
+  const handleEnter = (e) => {
+    if (e.key === 'Enter') {
+      // Perform sentiment analysis here and set the result to 'positive'
+      setUserOutput('positive');
     }
+  }
+
   return (
-    <>
-   <div className='full-container'>
+    <div className='full-container'>
+      <div>
+        <div className='heading-section'>Sentimental Analysis</div>
+      </div>
 
-    <div>
-        <div className='heading-section'> Sentimental Analysis</div>
-    </div>
-
-    <div className='sentimental-card-flexbox-container'>
+      <div className='sentimental-card-flexbox-container'>
         <div className='sentimental-input-section'>
-                <div>
-                 Input -section
-                </div>
-               <div>
-                <input type="text" value={userinput} placeholder='enter text' onChange={(e)=>inputchange(e)} className='input-text-div'/>
-               </div>
+          <div style={{ color: 'white' }}>Input -section</div>
+          <div style={{ color: 'white' }} className='enter-name-div'>
+            Enter input:
+          </div>
+          <div>
+            <input
+              type="text"
+              value={userinput}
+              placeholder='enter text'
+              onChange={(e) => inputchange(e)}
+              onKeyDown={(e) => handleEnter(e)} // Call handleEnter on key down
+              className='input-text-div'
+            />
+          </div>
         </div>
         <div className='sentimental-output-section'>
-        <div>
-                 Output -section
-                </div>
-                <div className='final-sentimental-div'>
-                    Finalsentiment
-                </div>
-               <div>
-                <input type="text" value={userOutput} className='output-text-div' />
-               </div>
+          <div style={{ color: 'white' }}>Output -section</div>
+          <div className='final-sentimental-div' style={{ color: 'white' }}>
+                 sentimental Analysis Output
+          </div>
+          <div>
+            <input
+              type="text"
+              value={userOutput}
+              className='output-text-div'
+              readOnly // Make the output field read-only
+            />
+          </div>
         </div>
+      </div>
     </div>
-
-   </div>
-    </>
-  )
+  );
 }
 
-export default Sentimental
+export default Sentimental;
